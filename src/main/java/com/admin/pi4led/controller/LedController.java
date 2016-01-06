fe841979b8e059078c9bfb23df62f55d18a22cd4
Laurinp <lifegoodtcom@gmail.com>
2016-01-06 01:46:45 +0100

    commit message

---
+++ src/main/java/com/admin/pi4led/controller/LedController.java
@@ -0,0 +1,29 @@
+package com.admin.pi4led.controller;
+
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+
+@RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
+    }
+    @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLeD", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
+    }
+
+}
